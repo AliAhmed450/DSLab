6d2cf35cce7a4bf9767e443226accf7cad1908ef
AliAhmed450 <aliahmed45059@gmail.com>
2025-12-25 18:14:30 +0500

    Second Task Completed

---
+++ task2/task2.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int findPartition(int arr[],int left,int right)
+{
+	int mid = (left + right)/2;
+	if(arr[mid] < 0)
+	{
+		if(arr[mid - 1] > -1)
+		{
+			return mid;
+		}
+		return findPartition(arr,left,mid);
+	}
+	return findPartition(arr,mid,right);
+}
+void rearrangearray(int arr[],int start,int end)
+{
+	int temp = arr[end];
+	for( int i = end; i > start;i--)
+	{
+		swap(arr[i],arr[i-1]);
+	}
+	arr[start] = temp; 
+}
+void rearrage(int arr[],const int size)
+{
+	int partition = findPartition(arr,0,size - 1);
+	
+	int loopCount = size - partition;
+	int start = 0;
+	int end = partition;
+	for(int i = 0; i < loopCount;i++)
+	{
+		rearrangearray(arr,start,end);
+		start++;
+		end++;
+	}
+}
+int main()
+{
+	const int size = 10;
+	int arr[size] = {1,5,7,8,15,16,26,-7,-22,-67};
+
+	rearrage(arr,size);
+	for(auto x : arr)
+	{
+		cout << x << " " ;
+	}
+}
